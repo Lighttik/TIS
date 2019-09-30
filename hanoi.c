81430f6897df868725f021b0216a2b59197849f4
unknown <BenkovaVeronika>
2019-09-30 20:49:19 +0200

    oprava

--- hanoi.c
+++ hanoi.c
@@ -7,7 +7,7 @@ void hanoi(int n, int a, int b)
   else
   {
     hanoi(n - 1, a, 6 - a - b);
-    printf("move stone %d from %d to %d\n", n, a, b);
+    printf("preloz kamen %d z %d na %d\n", n, a, b);
     hanoi(n - 1, 6 - a - b, b);
   }
 }
